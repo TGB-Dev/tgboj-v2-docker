f5dedc568ed2bb859c59f845d631f1850158aecc
Hoàng Minh Thiên <hoangminhthien05022009@gmail.com>
2025-02-07 16:40:24 +0700

    fix: SECRET_KEY issue in dmoj/config/local_settings.py

--- dmoj/config/local_settings.py
+++ dmoj/config/local_settings.py
@@ -10,7 +10,10 @@
 # SECURITY WARNING: keep the secret key used in production secret!
 # You may use this command to generate a key:
 # python3 -c 'from django.core.management.utils import get_random_secret_key;print(get_random_secret_key())'
-SECRET_KEY = os.environ.get("SECRET_KEY", "")
+# The secret key can't be obtained from the environment variable, so hardcoding
+# is the only way
+# SECRET_KEY = os.environ.get("SECRET_KEY", "")
+SECRET_KEY = "<YOUR_SECRET_KEY>"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "0") == "1"
 HOST = os.environ.get("HOST", "localhost")
